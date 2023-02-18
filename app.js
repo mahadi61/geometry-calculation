334992e1e69152a39a417553e4dd6c1d9511627d
Mahadi Hasan <mahadihasan8461@gmail.com>
2023-02-18 12:53:35 +0600

    random color added

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
 // edit icon to visible the input field
 
-// parallelogram
+// parallelogram edit icon
 document.getElementById('parallelogram-edit-icon').addEventListener('click', function(){
 const parallelogramElement = document.getElementById('parallelogram-input');
 parallelogramElement.style.display = 'inline-block';
 })
-// rhombus
+// rhombus edit icon
 document.getElementById('rhombus-edit-icon').addEventListener('click', function(){
 const rhombusElement = document.getElementById('rhombus-input');
 rhombusElement.style.display = 'inline-block';
 })
+// Pentagon edit icon
+document.getElementById('pentagon-edit-icon').addEventListener('click', function(){
+const pentagonElement = document.getElementById('pentagon-input');
+pentagonElement.style.display = 'inline-block';
+})
+// Ellipse edit icon
+document.getElementById('ellipse-edit-icon').addEventListener('click', function(){
+const ellipseElement = document.getElementById('ellipse-input');
+ellipseElement.style.display = 'inline-block';
+})
 
 
-// cart 1
+
+// cart 1 (triangle)
 document.getElementById('cart-1-btn').addEventListener('click', function(){
     const geometryTitle = elementTextValue('card-1-title');
     const valueOfB = getElementValueById('triangle-b');
@@ -33,7 +44,7 @@ document.getElementById('cart-1-btn').addEventListener('click', function(){
     tableBody.appendChild(tr);
     
 })
-// cart 2
+// cart 2 (rectangle)
 document.getElementById('cart-2-btn').addEventListener('click', function(){
     const geometryTitle = elementTextValue('cart-2-title');
     const valueOfW = getElementValueById('rectangle-w');
@@ -55,7 +66,7 @@ document.getElementById('cart-2-btn').addEventListener('click', function(){
     
 })
 
-// cart 3
+// cart 3 (parallelogram)
 document.getElementById('cart-3-btn').addEventListener('click', function(){
     const geometryTitle = elementTextValue('cart-3-title');
     const valueOfB = getElementValueById('parallelogram-b');
@@ -75,4 +86,93 @@ document.getElementById('cart-3-btn').addEventListener('click', function(){
     `;
     tableBody.appendChild(tr);
     
+})
+// cart 4 (rhombus)
+document.getElementById('cart-4-btn').addEventListener('click', function(){
+    const geometryTitle = elementTextValue('cart-4-title');
+    const valueOfD1 = getElementValueById('rhombus-d-1');
+    const valueOfD2 = getElementValueById('rhombus-d-2');
+    if(isNaN(valueOfD1) || isNaN(valueOfD2) || valueOfD1 <= 0 || valueOfD1 <= 0){
+        alert ('This is not a valid number')
+    }
+    const areaOfRhombus = 0.5 * valueOfD1 * valueOfD2;
+    const tableBody = document.getElementById('t-body');
+
+    const tr = document.createElement('tr');
+    tr.innerHTML= `
+    <td>${1}<td>
+    <td>${geometryTitle}<td>
+    <td>${areaOfRhombus}<td>
+    
+    `;
+    tableBody.appendChild(tr);
+    
+})
+// cart 5 (Pentagon)
+document.getElementById('cart-5-btn').addEventListener('click', function(){
+    const geometryTitle = elementTextValue('cart-5-title');
+    const valueOfP = getElementValueById('pentagons-p');
+    const valueOfB = getElementValueById('pentagons-b');
+    if(isNaN(valueOfP) || isNaN(valueOfB) || valueOfP <= 0 || valueOfB <= 0){
+        alert ('This is not a valid number')
+    }
+    const areaOfPentagon = 0.5 * valueOfP * valueOfB;
+    const tableBody = document.getElementById('t-body');
+
+    const tr = document.createElement('tr');
+    tr.innerHTML= `
+    <td>${1}<td>
+    <td>${geometryTitle}<td>
+    <td>${areaOfPentagon}<td>
+    
+    `;
+    tableBody.appendChild(tr);
+    
+})
+// cart 6 (Ellipse)
+document.getElementById('cart-6-btn').addEventListener('click', function(){
+    const geometryTitle = elementTextValue('cart-6-title');
+    const valueOfB = getElementValueById('ellipse-b');
+    const valueOfH = getElementValueById('ellipse-h');
+    if(isNaN(valueOfB) || isNaN(valueOfH) || valueOfB <= 0 || valueOfH <= 0){
+        alert ('This is not a valid number')
+    }
+    const areaOfEllipse = 3.1416 * valueOfB * valueOfH;
+    const tableBody = document.getElementById('t-body');
+
+    const tr = document.createElement('tr');
+    tr.innerHTML= `
+    <td>${1}<td>
+    <td>${geometryTitle}<td>
+    <td>${areaOfEllipse}<td>
+    
+    `;
+    tableBody.appendChild(tr);
+    
+})
+
+// random color in every card
+// cart - 1
+document.getElementById('cart-1-bg').addEventListener('mousemove', function(){
+    cardBgChange('cart-1-bg');
+})
+// cart - 2
+document.getElementById('cart-2-bg').addEventListener('mousemove', function(){
+    cardBgChange('cart-2-bg');
+})
+// cart - 3
+document.getElementById('cart-3-bg').addEventListener('mousemove', function(){
+    cardBgChange('cart-3-bg');
+})
+// cart - 4
+document.getElementById('cart-4-bg').addEventListener('mousemove', function(){
+    cardBgChange('cart-4-bg');
+})
+// cart - 5
+document.getElementById('cart-5-bg').addEventListener('mousemove', function(){
+    cardBgChange('cart-5-bg');
+})
+// cart - 6
+document.getElementById('cart-6-bg').addEventListener('mousemove', function(){
+    cardBgChange('cart-6-bg');
 })
--- utilities.js
+++ utilities.js
@@ -5,7 +5,22 @@ function getElementValueById(elementId){
     const elementValue = parseFloat(elementString);
     return elementValue;
 }
+// bring the text form a element
 function elementTextValue(elementId){
     const firstElement = document.getElementById(elementId).innerText;
     return firstElement
+}
+// random color generator
+function generateRGBColor(){
+    const red = Math.floor(Math.random()*255);
+    const green = Math.floor(Math.random()*255);
+    const blue = Math.floor(Math.random()*255);
+
+    return `rgb(${red}, ${green}, ${blue})`;
+}
+
+function cardBgChange(elementId){
+    const color = generateRGBColor();
+    const cardBg = document.getElementById(elementId);
+    cardBg.style.backgroundColor = color
 }
